4b44323b62b88caa82f1ef5a71823acaea572fe2
Tim Goll <tim@timgoll.de>
2017-11-15 20:57:22 +0100

    erste tests

---
+++ advent.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+
+    simpleAJAX.request(null, 'riddle.json', response);
+
+    var response = function(data) {
+        console.log("response");
+        https://gist.github.com/thiagodebastos/08ea551b97892d585f17
+        console.log(data);
+    };
+
+};
